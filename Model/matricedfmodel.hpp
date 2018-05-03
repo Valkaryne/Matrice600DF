9a983f53324c4ec8f0856af1aac77161cfc4f232
Valkaryne <anix.mig29@gmail.com>
2018-05-03 15:17:18 +0300

    started data linkage

--- Model/matricedfmodel.hpp
+++ Model/matricedfmodel.hpp
@@ -10,20 +10,32 @@ class MatriceDFModel : public ModelInterface
 public:
     MatriceDFModel();
 
-    void initializeModel();
+    /* Setters */
+    void setCalibrationMode(bool setCalibration);
+
+    /* Getters */
     UdpChannel* getUDPChannel();
     TcpChannel* getTCPChannel();
+
+    /* Other */
+    void initializeModel();
 
 private slots:
     void samplesHandler(const QVector<double> samplesAm1, const QVector<double> samplesAm2,
                         const QVector<double> samplesPh);
+    void polarSamplesHandler(const QVector<double> samplesAm1, const QVector<double> samplesAm2,
+                             const QVector<double> samplesPh);
 
 signals:
     void amplitudeSamplesReady(const QVector<double> amplitudeOne, const QVector<double> amplitudeTwo);
     void phaseSamplesReady(const QVector<double> phaseDiff);
+    void polarSamplesReady(const int az, const double rado, const double radl, const double phase);
+    void polarSamplesReady(const int az, const double rado, const double phase);
 
 private:
-    double gain;
+    double am1N, am2N, amSN;
+
+    QVector<double> simpleAm1, simpleAm2, summAm;
 };
 
 #endif // MATRICEDFMODEL_HPP
--- Model/modelinterface.hpp
+++ Model/modelinterface.hpp
@@ -18,17 +18,48 @@ public:
     virtual UdpChannel* getUDPChannel() = 0;
     virtual TcpChannel* getTCPChannel() = 0;
 
+    /* Setters */
+    virtual void setRangeBounds(QVector<int> rangeBounds) {
+        if (this->rangeBounds != rangeBounds)
+            this->rangeBounds = rangeBounds;
+    }
+
+    virtual void setCalibrationMode(bool setCalibration) {
+        if (this->setCalibration != setCalibration)
+            this->setCalibration = setCalibration;
+    }
+
+    virtual void setDrawingMode(bool doubleChannel) {
+        if (this->doubleChannel != doubleChannel)
+            this->doubleChannel = doubleChannel;
+    } // NOTE: is it possible to use a strategy-pattern?
+
+    virtual void setCurrentGain(double gain) {
+        if (this->gain != gain)
+            this->gain = gain;
+    }
+
 public slots:
     virtual void samplesHandler(const QVector<double> samplesAm1, const QVector<double> samplesAm2,
                                 const QVector<double> samplesPh) = 0;
+    virtual void polarSamplesHandler(const QVector<double> samplesAm1, const QVector<double> samplesAm2,
+                                     const QVector<double> samplesPh) = 0;
 
 signals:
     virtual void amplitudeSamplesReady(const QVector<double> amplitudeOne, const QVector<double> amplitudeTwo) = 0;
     virtual void phaseSamplesReady(const QVector<double> phaseDiff) = 0;
+    virtual void polarSamplesReady(const int az, const double rado, const double phase) = 0;
+    virtual void polarSamplesReady(const int az, const double rado, const double radl, const double phase) = 0;
 
 protected:
     UdpChannel  *udpChannel;
     TcpChannel  *tcpChannel;
+
+    double gain;
+    bool setCalibration;
+    bool doubleChannel;
+
+    QVector<int> rangeBounds;
 };
 
 #endif // MODELINTERFACE_HPP
